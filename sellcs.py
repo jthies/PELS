15efff0e7031fadb2adbfcb15540123579bea1b5
Jonas Thies <16190001+jthies@users.noreply.github.com>
2024-04-24 12:01:59 +0200

    jit-compile SELL-C-sigma conversion to make it a bit more fun to experiment with (was REALLY slow)

--- sellcs.py
+++ sellcs.py
@@ -32,66 +32,15 @@ class sellcs_matrix:
         self.sigma = sigma
 
         if A_csr != None:
-            self.from_csr(A_csr)
+            self.shape = A_csr.shape
+            self.nnz = A_csr.nnz
+            self.indptr, self.data, self.indices, self.permute, self.unpermute = csr_to_sellcs(A_csr.indptr, A_csr.data, A_csr.indices, C, sigma)
         elif A_arrays != None:
             self.data, self.indices, self.indptr, self.permute, self.unpermute, self.nnz = A_arrays
-            self.nchunks = self.indptr.size-1
             self.shape = shape
-            self.dtype = self.data.dtype
-
-    def from_csr(self, A_csr):
-
-        self.shape = A_csr.shape
-        self.dtype = A_csr.dtype
-
-        C = self.C
-        sigma = self.sigma
-
-        nrows = self.shape[0]
-        nchunks = (nrows+C-1)//C
-        self.nchunks = nchunks
-        self.indptr  = np.zeros(nchunks+1, dtype='int64')
-        self.permute = np.arange(nrows)
-        self.unpermute = np.empty_like(self.permute)
-        self.nnz = A_csr.nnz
-        scope = max(C,sigma)
-
-        chunk = 0
-
-        for row_offset in range(0,nrows, scope):
-            last = min(row_offset+scope, nrows)
-            rng0=range(row_offset, last)
-            rng1=range(row_offset+1, last+1)
-            row_len = A_csr.indptr[rng1] - A_csr.indptr[rng0]
-            # Note: the only way to make numpy sort in descending order is to
-            #       give it the negative values and sort ascending :)
-            if sigma>1:
-                idx = np.argsort(-row_len)
-                row_len = row_len[idx]
-                self.permute[rng0] = row_offset+idx
-            for i in range(row_offset, last, C):
-                c = min(C, nrows-i)
-                w = max(row_len[i-row_offset:i-row_offset+c])
-                self.indptr[chunk+1] = self.indptr[chunk] + c*w
-                chunk += 1
-
-        nnz_stored = self.indptr[nchunks]
-        self.unpermute[self.permute] = np.arange(nrows)
-        self.data = np.zeros(nnz_stored, dtype=A_csr.data.dtype)
-        self.indices = np.zeros(nnz_stored, dtype=A_csr.indices.dtype)
-
-        for chunk in range(nchunks):
-            c = min(C*(chunk+1), nrows)-C*chunk
-            i0 = C*chunk
-            i1 = i0 + c
-            for i in range(i0, i1):
-                rowA = self.permute[i]
-                k0 = A_csr.indptr[rowA]
-                k1 = A_csr.indptr[rowA+1]
-                for k in range(k0, k1):
-                    pos  = self.indptr[chunk] + (k-k0)*c + (i-i0)
-                    self.data[pos] = A_csr.data[k]
-                    self.indices[pos] = self.unpermute[A_csr.indices[k]]
+        self.dtype = self.data.dtype
+        self.nchunks = self.indptr.size-1
+        self.nnz_stored = self.indptr[self.nchunks]
 
     def __str__(self):
         out = 'SELL-%d-%d matrix, shape=[%d %d]\n'%(self.C,self.sigma, self.shape[0],self.shape[1])
@@ -102,3 +51,59 @@ class sellcs_matrix:
         out += 'indices='+str(self.indices)+'\n'
         out += 'data='+str(self.data)+'\n'
         return out
+
+@jit(nopython=True, parallel=True)
+def csr_to_sellcs(A_indptr, A_data, A_indices, C, sigma):
+    '''
+    internal utility function to efficiently convert CSR matrix arrays to SELL-C-sigma format.
+    Users should not call this but use sellcs_matrix(A_csr=A) instead.
+
+    Returns: indptr, data, indices, permute, unpermute
+    '''
+    nrows = A_indptr.size-1
+    nchunks = (nrows+C-1)//C
+    indptr  = np.zeros(nchunks+1, dtype='int64')
+    permute = np.arange(nrows)
+    unpermute = np.empty_like(permute)
+    scope = max(C,sigma)
+
+    chunk = 0
+
+    for row_offset in range(0,nrows, scope):
+        last = min(row_offset+scope, nrows)
+        # numba doesn't allow indexing with a range, so we make rng0/1 arrays for now:
+        #rng0=range(row_offset, last)
+        #rng1=range(row_offset+1, last+1)
+        rng0=np.arange(row_offset, last)
+        rng1=np.arange(row_offset+1, last+1)
+        row_len = A_indptr[rng1] - A_indptr[rng0]
+        # Note: the only way to make numpy sort in descending order is to
+        #       give it the negative values and sort ascending :)
+        if sigma>1:
+            idx = np.argsort(-row_len)
+            row_len = row_len[idx]
+            permute[rng0] = row_offset+idx
+        for i in range(row_offset, last, C):
+            c = min(C, nrows-i)
+            w = max(row_len[i-row_offset:i-row_offset+c])
+            indptr[chunk+1] = indptr[chunk] + c*w
+            chunk += 1
+
+    nnz_stored = indptr[nchunks]
+    unpermute[permute] = np.arange(nrows)
+    data = np.zeros(nnz_stored, dtype=A_data.dtype)
+    indices = np.zeros(nnz_stored, dtype=A_indices.dtype)
+
+    for chunk in prange(nchunks):
+        c = min(C*(chunk+1), nrows)-C*chunk
+        i0 = C*chunk
+        i1 = i0 + c
+        for i in range(i0, i1):
+            rowA = permute[i]
+            k0 = A_indptr[rowA]
+            k1 = A_indptr[rowA+1]
+            for k in range(k0, k1):
+                pos  = indptr[chunk] + (k-k0)*c + (i-i0)
+                data[pos] = A_data[k]
+                indices[pos] = unpermute[A_indices[k]]
+    return indptr, data, indices, permute, unpermute
